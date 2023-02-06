34f26299ef55cd9a30bd1a970d14e7a801e4cbfb
mauroapjr <mauroapjr@outlook.com>
2023-02-06 18:24:32 -0500

    Finish challenge and add more data to repository

--- 1_queries/1_get_allNames.sql
+++ 1_queries/1_get_allNames.sql
@@ -1,34 +1,34 @@
 -- Get the names of all of the students from a single cohort.
--- SELECT id, name, cohort_id 
--- FROM students
--- WHERE cohort_id = 1
--- ORDER BY cohort_id;
+SELECT id, name, cohort_id 
+FROM students
+WHERE cohort_id = 1
+ORDER BY cohort_id;
 
 -- Select the total number of students who were in the first 3 cohorts.
--- SELECT id, name, cohort_id 
--- FROM students
--- WHERE cohort_id <= 3
--- ORDER BY cohort_id;
+SELECT id, name, cohort_id 
+FROM students
+WHERE cohort_id <= 3
+ORDER BY cohort_id;
 
 -- Get all of the students that don't have an email or phone number.
--- SELECT id, name, cohort_id 
--- FROM students
--- WHERE phone IS NULL OR email IS NULL
--- ORDER BY cohort_id;
+SELECT id, name, cohort_id 
+FROM students
+WHERE phone IS NULL OR email IS NULL
+ORDER BY cohort_id;
 
 -- Get all of the students without a gmail.com account and a phone number.
--- SELECT id, name, email, cohort_id 
--- FROM students
--- WHERE phone IS NULL AND email NOT LIKE '%gmail.com'
--- ORDER BY cohort_id;
+SELECT id, name, email, cohort_id 
+FROM students
+WHERE phone IS NULL AND email NOT LIKE '%gmail.com'
+ORDER BY cohort_id;
 
 -- Get all of the students currently enrolled.
--- SELECT id, name, cohort_id 
--- FROM students
--- WHERE end_date IS NULL
--- ORDER BY cohort_id;
+SELECT id, name, cohort_id 
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
 
 -- Get all graduates without a linked Github account.
--- SELECT name, email, phone
--- FROM students
--- WHERE end_date IS NOT NULL AND github IS NULL;
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL AND github IS NULL;
