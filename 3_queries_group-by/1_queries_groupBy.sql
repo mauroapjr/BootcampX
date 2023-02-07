6178354cf926d08efeaa24a1a53da27f916aaec5
mauroapjr <mauroapjr@outlook.com>
2023-02-07 18:29:02 -0500

    Add another file and download data

--- 3_queries_group-by/1_queries_groupBy.sql
+++ 3_queries_group-by/1_queries_groupBy.sql
@@ -37,15 +37,59 @@
 
 -- Get the students who's average time it takes to complete an assignment is less 
 -- than the average estimated time it takes to complete an assignment.
-SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
-FROM students
-JOIN assignment_submissions ON student_id = students.id
-JOIN assignments ON assignments.id = assignment_id
-WHERE end_date IS NULL
-GROUP BY student
-HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-ORDER BY average_assignment_duration;
+-- SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+-- FROM students
+-- JOIN assignment_submissions ON student_id = students.id
+-- JOIN assignments ON assignments.id = assignment_id
+-- WHERE end_date IS NULL
+-- GROUP BY student
+-- HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+-- ORDER BY average_assignment_duration;
 
 
 
 
+
+-- SELECT (
+--   SELECT count(assignments)
+--   FROM assignments
+-- )-count(assignment_submissions) as total_incomplete
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
+
+-- SELECT count(students)
+-- FROM students
+-- JOIN cohorts on cohorts.id = cohort_id
+-- GROUP BY cohorts;
+
+-- SELECT avg(total_students) as average_students
+-- FROM (
+--   SELECT count(students) as total_students
+--   FROM students
+--   JOIN cohorts on cohorts.id = cohort_id
+--   GROUP BY cohorts
+-- ) as totals_table;
+
+-- SELECT avg(total_students) as average_students
+-- FROM (
+--   SELECT count(students) as total_students, cohorts.name as cohort_name
+--   FROM students
+--   JOIN cohorts on cohorts.id = cohort_id
+--   GROUP BY cohorts.name
+-- ) as totals_table;
+
+-- SELECT assignment_id
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Eunice Morar';
+
+-- SELECT assignments.name
+-- FROM assignments 
+-- WHERE id NOT IN
+-- (
+--   SELECT assignment_id
+--   FROM assignment_submissions
+--   JOIN students ON students.id = student_id
+--   WHERE students.name = 'Ibrahim Schimmel'
+-- );
