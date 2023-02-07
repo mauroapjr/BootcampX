b0249f7ea12219d87dacbcdf671bc68dbe65af1f
mauroapjr <mauroapjr@outlook.com>
2023-02-07 15:29:57 -0500

    Add another file and completed task 3 HAVING, JOIN and GROUP BY

--- 2_queries_joins/1_assignment_time.sql
+++ 2_queries_joins/1_assignment_time.sql
@@ -1,12 +1,13 @@
 -- Get the total amount of time that a student has spent on all assignments.
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.name = 'Jessika Jenkins';
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Jessika Jenkins';
 
 -- Get the total amount of time that all students from a specific cohort have spent on all assignments.
-SELECT sum(assignment_submissions.duration) as total_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'FEB12';
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- WHERE cohorts.name = 'FEB12';
+
